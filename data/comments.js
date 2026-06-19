d07be28d23d44604f64adf8e532a699e02e04153
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-06-18 21:49:57 -0400

    Created Repository

---
+++ data/comments.js
@@ -0,0 +1,22 @@
+const comments = [
+  {
+    id: 1,
+    userId: 1,
+    postId: 1,
+    body: "Great post!",
+  },
+  {
+    id: 2,
+    userId: 2,
+    postId: 1,
+    body: "I agree with this.",
+  },
+  {
+    id: 3,
+    userId: 3,
+    postId: 2,
+    body: "This was helpful.",
+  },
+];
+
+export default comments;
